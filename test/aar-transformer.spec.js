b256fb6c11a2fbfb35cba6e6d7acebca2af29169
Chris Williams <chris.a.williams@gmail.com>
2020-10-16 16:11:42 -0400

    style: fix eslint issues

--- test/aar-transformer.spec.js
+++ test/aar-transformer.spec.js
@@ -7,139 +7,139 @@ const { AarTransformer } = require('../index');
 const outputPath = path.resolve(__dirname, 'output');
 
 describe('AarTransformer',  () => {
-    describe('transform', () => {
-        let transformer = null;
+	describe('transform', () => {
+		let transformer = null;
 
-        function transform(options, callback) {
-            const defaultOptions = {
-                aarPathAndFilename: path.resolve(__dirname, 'fixtures', 'com.example.testlibrary.aar'),
-                outputPath: outputPath
-            }
-            const mergedOptions = Object.assign({}, defaultOptions, options);
-            transformer.transform(mergedOptions, callback);
-        }
+		function transform(options, callback) {
+			const defaultOptions = {
+				aarPathAndFilename: path.resolve(__dirname, 'fixtures', 'com.example.testlibrary.aar'),
+				outputPath: outputPath
+			};
+			const mergedOptions = Object.assign({}, defaultOptions, options);
+			transformer.transform(mergedOptions, callback);
+		}
 
-        beforeEach(done => {
-            transformer = new AarTransformer();
-            fs.mkdir(outputPath, done);
-        });
+		beforeEach(done => {
+			transformer = new AarTransformer();
+			fs.mkdir(outputPath, done);
+		});
 
-        afterEach(done => {
-            rimraf(outputPath, done);
-        });
+		afterEach(done => {
+			rimraf(outputPath, done);
+		});
 
-        it('should require input file and output path options', done => {
-            const invalidOptions = [
-                {},
-                { aarPathAndFilename: 'foo' },
-                { outputPath: 'bar' }
-            ]
-            Promise.all(invalidOptions.map(options => {
-                return new Promise(resolve => {
-                    transformer = new AarTransformer();
-                    transformer.transform(options, (err, result) => {
-                        expect(err).toEqual(jasmine.any(Error));
+		it('should require input file and output path options', done => {
+			const invalidOptions = [
+				{},
+				{ aarPathAndFilename: 'foo' },
+				{ outputPath: 'bar' }
+			];
+			Promise.all(invalidOptions.map(options => {
+				return new Promise(resolve => {
+					transformer = new AarTransformer();
+					transformer.transform(options, (err, result) => {
+						expect(err).toEqual(jasmine.any(Error));
 
-                        resolve();
-                    });
-                });
-            })).then(done).catch(err => console.error(err));
-        });
+						resolve();
+					});
+				});
+			})).then(done).catch(err => console.error(err));
+		});
 
-        it('should extract contents to output path', done => {
-            transform({}, (err, result) => {
-                expect(err).toBeNull();
-                expect(result).toBeTruthy();
-                expect(fs.existsSync(result.explodedPath)).toBeTruthy();
+		it('should extract contents to output path', done => {
+			transform({}, (err, result) => {
+				expect(err).toBeNull();
+				expect(result).toBeTruthy();
+				expect(fs.existsSync(result.explodedPath)).toBeTruthy();
 
-                done();
-            });
-        });
+				done();
+			});
+		});
 
-        it('should read package name from Android manifest', done => {
-            transform({}, (err, result) => {
-                expect(err).toBeNull();
-                expect(result).toBeTruthy();
-                expect(result.packageName).toEqual('com.example.testlibrary');
+		it('should read package name from Android manifest', done => {
+			transform({}, (err, result) => {
+				expect(err).toBeNull();
+				expect(result).toBeTruthy();
+				expect(result.packageName).toEqual('com.example.testlibrary');
 
-                done();
-            });
-        });
+				done();
+			});
+		});
 
-        it('should register Java libraries', done => {
-            transform({}, (err, result) => {
-                expect(err).toBeNull();
-                expect(result).toBeTruthy();
-                expect(result.jars.length).toEqual(2);
-                expect(result.jars[0]).toEqual(path.join(result.explodedPath, 'classes.jar'));
-                expect(result.jars[1]).toEqual(path.join(result.explodedPath, 'libs', 'json-20180130.jar'));
+		it('should register Java libraries', done => {
+			transform({}, (err, result) => {
+				expect(err).toBeNull();
+				expect(result).toBeTruthy();
+				expect(result.jars.length).toEqual(2);
+				expect(result.jars[0]).toEqual(path.join(result.explodedPath, 'classes.jar'));
+				expect(result.jars[1]).toEqual(path.join(result.explodedPath, 'libs', 'json-20180130.jar'));
 
-                done();
-            });
-        });
+				done();
+			});
+		});
 
-        it('should copy Java libraries if destination path sepcified', done => {
-            const destPath = path.join(outputPath, 'dest');
-            transform({ libraryDestinationPath: destPath }, (err, result) => {
-                expect(err).toBeNull();
-                expect(result).toBeTruthy();
-                expect(result.jars.length).toEqual(2);
-                expect(fs.existsSync(path.join(destPath, `${result.packageName}.jar`))).toBeTruthy();
-                expect(fs.existsSync(path.join(destPath, path.basename(result.jars[1])))).toBeTruthy();
+		it('should copy Java libraries if destination path sepcified', done => {
+			const destPath = path.join(outputPath, 'dest');
+			transform({ libraryDestinationPath: destPath }, (err, result) => {
+				expect(err).toBeNull();
+				expect(result).toBeTruthy();
+				expect(result.jars.length).toEqual(2);
+				expect(fs.existsSync(path.join(destPath, `${result.packageName}.jar`))).toBeTruthy();
+				expect(fs.existsSync(path.join(destPath, path.basename(result.jars[1])))).toBeTruthy();
 
-                done();
-            });
-        });
+				done();
+			});
+		});
 
-        it('should copy module assets if destination path specified', done => {
-            const destPath = path.join(outputPath, 'dest');
-            transform({ assetsDestinationPath: destPath }, (err, result) => {
-                expect(err).toBeNull();
-                expect(result).toBeTruthy();
-                
-                expect(fs.existsSync(path.join(destPath, 'logo-titanium.png'))).toBeTruthy();
+		it('should copy module assets if destination path specified', done => {
+			const destPath = path.join(outputPath, 'dest');
+			transform({ assetsDestinationPath: destPath }, (err, result) => {
+				expect(err).toBeNull();
+				expect(result).toBeTruthy();
 
-                done();
-            });
-        });
+				expect(fs.existsSync(path.join(destPath, 'logo-titanium.png'))).toBeTruthy();
 
-        it('should copy native libraries if destination path sepcified', done => {
-            const destPath = path.join(outputPath, 'dest');
-            const nativeLibName = 'libnative-lib.so';
-            transform({ sharedLibraryDestinationPath: destPath }, (err, result) => {
-                expect(err).toBeNull();
-                expect(result).toBeTruthy();
-                expect(result.nativeLibraries.length).toEqual(4);
-                fs.readdir(destPath, (err, entries) => {
-                    expect(err).toBeNull();
-                    const expectedAbis = ['arm64-v8a', 'armeabi-v7a', 'x86', 'x86_64'];
-                    expectedAbis.forEach(abiName => {
-                        expect(entries).toContain(abiName);
-                        const expectedLibDestPath = path.join(destPath, abiName, nativeLibName);
-                        expect(fs.existsSync(expectedLibDestPath)).toBeTruthy();
-                    });
+				done();
+			});
+		});
 
-                    done();
-                });
-            });
-        });
+		it('should copy native libraries if destination path sepcified', done => {
+			const destPath = path.join(outputPath, 'dest');
+			const nativeLibName = 'libnative-lib.so';
+			transform({ sharedLibraryDestinationPath: destPath }, (err, result) => {
+				expect(err).toBeNull();
+				expect(result).toBeTruthy();
+				expect(result.nativeLibraries.length).toEqual(4);
+				fs.readdir(destPath, (err, entries) => {
+					expect(err).toBeNull();
+					const expectedAbis = [ 'arm64-v8a', 'armeabi-v7a', 'x86', 'x86_64' ];
+					expectedAbis.forEach(abiName => {
+						expect(entries).toContain(abiName);
+						const expectedLibDestPath = path.join(destPath, abiName, nativeLibName);
+						expect(fs.existsSync(expectedLibDestPath)).toBeTruthy();
+					});
 
-        it('should throw error if Android archive does not exist', done => {
-            transform({ aarPathAndFilename: 'not-there.aar' }, (err, result) => {
-                expect(err).toEqual(jasmine.any(Error));
+					done();
+				});
+			});
+		});
 
-                done();
-            });
-        });
+		it('should throw error if Android archive does not exist', done => {
+			transform({ aarPathAndFilename: 'not-there.aar' }, (err, result) => {
+				expect(err).toEqual(jasmine.any(Error));
 
-        it('should throw error if same transsformer is used more than once', done => {
-            transform({}, (err, result) => {
-                expect(err).toBeNull();
-                expect(result).toBeTruthy();
-                
-                expect(() => transform({}, done)).toThrowError();
-                done();
-            })
-        });
-    });
+				done();
+			});
+		});
+
+		it('should throw error if same transsformer is used more than once', done => {
+			transform({}, (err, result) => {
+				expect(err).toBeNull();
+				expect(result).toBeTruthy();
+
+				expect(() => transform({}, done)).toThrowError();
+				done();
+			});
+		});
+	});
 });
--- test/symbol-writer.spec.js
+++ test/symbol-writer.spec.js
@@ -10,34 +10,34 @@ const lib2SymbolsPath = path.join(fixturesPath, 'library-symbols2.txt');
 const fullSymbolsPath = path.join(fixturesPath, 'full-symbols.txt');
 
 function loadSymbols(symbolsPath) {
-    const loader = new SymbolLoader(symbolsPath);
-    loader.load();
-    return loader;
+	const loader = new SymbolLoader(symbolsPath);
+	loader.load();
+	return loader;
 }
 
 describe('SymbolWriter', () => {
-    const outputPath = path.join(__dirname, 'output');
+	const outputPath = path.join(__dirname, 'output');
 
-    beforeEach(done => {
-        fs.mkdir(outputPath, done);
-    })
+	beforeEach(done => {
+		fs.mkdir(outputPath, done);
+	});
 
-    afterEach(done => {
-        rimraf(outputPath, done);
-    });
+	afterEach(done => {
+		rimraf(outputPath, done);
+	});
 
-    it('should write merged symbols', () => {
-        const lib1Symbols = loadSymbols(lib1SymbolsPath);
-        const lib2Symbols = loadSymbols(lib2SymbolsPath);
-        const fullValues = loadSymbols(fullSymbolsPath);
+	it('should write merged symbols', () => {
+		const lib1Symbols = loadSymbols(lib1SymbolsPath);
+		const lib2Symbols = loadSymbols(lib2SymbolsPath);
+		const fullValues = loadSymbols(fullSymbolsPath);
 
-        const writer = new SymbolWriter(outputPath, 'com.appc', fullValues);
-        writer.addSymbolsToWrite(lib1Symbols);
-        writer.addSymbolsToWrite(lib2Symbols);
-        writer.write();
+		const writer = new SymbolWriter(outputPath, 'com.appc', fullValues);
+		writer.addSymbolsToWrite(lib1Symbols);
+		writer.addSymbolsToWrite(lib2Symbols);
+		writer.write();
 
-        const expectedContent = fs.readFileSync(path.join(fixturesPath, 'R.java'));
-        const actualContent = fs.readFileSync(path.join(outputPath, 'com', 'appc', 'R.java'));
-        expect(actualContent).toEqual(expectedContent);
-    });
+		const expectedContent = fs.readFileSync(path.join(fixturesPath, 'R.java'));
+		const actualContent = fs.readFileSync(path.join(outputPath, 'com', 'appc', 'R.java'));
+		expect(actualContent).toEqual(expectedContent);
+	});
 });
